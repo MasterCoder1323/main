ce60ec4dbd4ccdf93a5bf279b51076ed3e7d1c9d
Master Coder <sparlevi18@gmail.com>
2023-05-19 08:17:33 -0400

    canvasTools Sprite

--- code/javascript/libraries/canvasTools.js
+++ code/javascript/libraries/canvasTools.js
@@ -1,4 +1,45 @@
-function canvas(id){
-    this.canvas = document.getElementById(id);
-    this.ctx = this.canvas.getContext("2d");
+function canvas(id = "myCanvas"){
+    this.properties1 = document.getElementById(id);
+    this.ctx = this.properties1.getContext("2d");
 };
+function sprite(x,y,width,height,image, location){
+    this.width = width;
+    this.height = height;
+    this.image = image;
+    this.imageTag = null;
+    this.x = x;
+    this.y = y;
+    this.canvas = location;
+    this.start = function(){
+        this.imageTag = new Image();
+        this.imageTag.src = this.image;
+        this.imageTag.onload = this.draw();
+    },
+    this.draw = function(){
+        this.canvas.ctx.drawImage(this.imageTag,this.x,this.y,this.width,this.height);
+    },
+    this.up = function(px = 10){
+        if(this.y >=0){
+            this.y = this.y - px;
+        }
+        this.draw();
+    },
+    this.down = function(px = 10){
+        if(this.y <=this.canvas.canvas.height){
+            this.y = this.y + px;
+        }
+        this.draw();
+    },
+    this.left = function(px = 10){
+        if(this.x >=0){
+            this.x = this.x - px;
+        }
+        this.draw();
+    },
+    this.right = function(px = 10){
+        if(this.x <=canvas1.width){
+            this.x = this.x + px;
+        }
+        this.draw();
+    }
+}
