f0393b298ee2cd9d296c6da8d9c5776504d86794
Master Coder <sparlevi18@gmail.com>
2023-05-29 08:24:58 -0400

    message

--- code/javascript/libraries/fabricTools.js
+++ code/javascript/libraries/fabricTools.js
@@ -1,69 +1,81 @@
-//Import Libraries
-import fabric from "./fabric.js"
-//Functions
-function jFabric(id = "myCanvas", backgroundImg = "imgName"){
-    this.properties1 = document.getElementById(id);
-    this.ctx = this.properties1.getContext("2d");
-    this.fabric = fabric.canvas(id);
-    if(backgroundImg !== null){
-    	this.addImage(0,0,this.properties1.width,this.properties1.height,backgroundImg,this)
-    }
-	this.addImage = function(x = 10,y = 10,width = 100,height = 100,url = URL) {
-		fabric.Image.fromURL(url,function(Img){
-			var Image = Img;
-			Image.scaleToWidth(width);
-			Image.scaleToHeight(height);
-			Image.set({
-				top: x,
-				left: y
-			});
-			this.fabric.add(Image);
-		});
-	}
-};
-function jSprite(x = 0,y = 0,width = 100,height = 100,image = "img", location = jFabric){
-    this.width = width;
-    this.height = height;
-    this.image = image;
-    this.x = x;
-    this.y = y;
-	this.canvas = location.properties1;
-    this.fabric = location.fabric;
-    this.draw = function(){
-        fabric.Image.fromURL(this.img,function(Img){
-            this.Image = Img;
-            this.Image.scaleToWidth(this.width);
-            this.Image.scaleToHeight(this.height);
-            this.Image.set({
-                top: this.x,
-                left: this.y
-            });
-            this.fabric.add(this.Image);
-        });
-    }
-    this.up = function(px = 10){
-        if(this.y >=0){
-            this.y = this.y - px;
-        }
-        this.draw();
-    },
-    this.down = function(px = 10){
-        if(this.y <=this.canvas.height){
-            this.y = this.y + px;
-        }
-        this.draw();
-    },
-    this.left = function(px = 10){
-        if(this.x >=0){
-            this.x = this.x - px;
-        }
-        this.draw();
-    },
-    this.right = function(px = 10){
-        if(this.x <=this.canvas.width){
-            this.x = this.x + px;
-        }
-        this.draw();
-    }
-}
+//Imports
 
+
+//Functions
+const jFabric = {
+    canvas: function(id, backgroundImg) {
+        this.properties1 = document.getElementById(id);
+        this.ctx = this.properties1.getContext("2d");
+        this.fabric = fabric.canvas(id);
+        if (backgroundImg !== null) {
+            this.addImage(0, 0, this.properties1.width, this.properties1.height, backgroundImg, this)
+        }
+        this.addImage = function(x = 10, y = 10, width = 100, height = 100, url = URL) {
+            fabric.Image.fromURL(url, function(Img) {
+                var Image = Img;
+                Image.scaleToWidth(width);
+                Image.scaleToHeight(height);
+                Image.set({
+                    top: x,
+                    left: y
+                });
+                this.fabric.add(Image);
+            });
+        }
+    },
+    sprite: function(x, y, width, height, image, location = jFabric, idNum = 10) {
+        this.width = width;
+        this.height = height;
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.canvas = location.properties1;
+        this.fabric = location.fabric;
+        this.location = location
+        this.spriteID = "sprite_" + idNum
+        this.draw = function() {
+            this.fabric.remove(this.spriteID);
+            fabric.Image.fromURL(url, function(Img) {
+                var Image = Img;
+                Image.scaleToWidth(width);
+                Image.scaleToHeight(height);
+                Image.set({
+                    top: x,
+                    left: y
+                });
+                this.spriteID = Image;
+                this.fabric.add(this.spriteID);
+            });
+        }
+        this.up = function(px = 10) {
+                if (this.y >= 0) {
+                    this.y = this.y - px;
+                }
+                this.draw();
+            },
+            this.down = function(px = 10) {
+                if (this.y <= this.canvas.height) {
+                    this.y = this.y + px;
+                }
+                this.draw();
+            },
+            this.left = function(px = 10) {
+                if (this.x >= 0) {
+                    this.x = this.x - px;
+                }
+                this.draw();
+            },
+            this.right = function(px = 10) {
+                if (this.x <= this.canvas.width) {
+                    this.x = this.x + px;
+                }
+                this.draw();
+            }
+    }
+};
+
+export { jFabric }
+/*
+To Import add:
+	
+*/
--- code/javascript/paper_minecraft/main.js
+++ code/javascript/paper_minecraft/main.js
@@ -1,70 +1,34 @@
-var canvas = new fabric.canvas("myCanvas");
-var player = {
-    img: "player.png",
-    Image: null,
-    width:150,
-    height:140,
-    x: 10,
-    y: 10,
-    draw: function(){
-        fabric.Image.fromURL(this.img,function(Img){
-            this.Image = Img;
-            this.Image.scaleToWidth(this.width);
-            this.Image.scaleToHeight(this.height);
-            this.Image.set({
-                top: this.x,
-                left: this.y
-            });
-            canvas.add(this.Image);
-        });
-    }
-}
-var move = {
-    Up:function(){
+//Import jFabric
+import { jFabric} from '../libraries/fabricTools.js';
 
-    },
-    Down:function(){
-
-    },
-    Left:function(){
-
-    },
-    Right:function(){
-
-    }
-}
+//Code
+var canvas = new jFabric.canvas("myCanvas");
+var player = new jFabric.sprite(10, 10, 140, 150, "player.png", canvas, "player");
+player.draw();
 var blockInfo = {
-    width:30,
-    height:30,
+    width: 30,
+    height: 30,
     currentImg: "ground.png"
 }
-var blockImage;
-function addBlock(x,y){
-    fabric.Image.fromURL(blockInfo.currentImg,function(Img){
-        blockImage = Img;
-        blockImage.scaleToWidth(blockInfo.width);
-        blockImage.scaleToHeight(blockInfo.height);
-        blcoImage.set({
-            top: x,
-            left: y
-        });
-        canvas.add(blockImage);
-    });
+
+function addBlock(x, y) {
+    canvas.addImage(x, y, blockInfo.width, blockInfo.height, blockInfo.currentImg);
 }
 window.addEventListener("keydown", minecraftKeyPress);
-function minecraftKeyPress(e){
+
+function minecraftKeyPress(e) {
     var keyCode = e.keyCode;
     console.log(keyCode);
     //block editing keys
     if (e.shiftKey && keyCode == "189") {
         blockInfo.width = blockInfo.width - 5;
         blockInfo.height = blockInfo.height - 5;
-        console.log("Block size redused to: "+blockInfo.width);
+        console.log("Block size redused to: " + blockInfo.width);
     }
     if (e.shiftKey && keyCode == "187") {
         blockInfo.width = blockInfo.width + 5;
         blockInfo.height = blockInfo.height + 5;
-        console.log("Block size incresed to: "+blockInfo.width);
+        console.log("Block size incresed to: " + blockInfo.width);
     }
     if (keyCode == "13") {
         placeBlock();
@@ -72,56 +36,56 @@ function minecraftKeyPress(e){
     }
     //moving keys
     if (keyCode == "38") {
-        move.Up();
+        player.up();
         console.log("Player went Up");
     }
     if (keyCode == "40") {
-        move.Down();
+        player.down();
         console.log("Player went Down");
     }
     if (keyCode == "37") {
-        move.Left();
+        player.left();
         console.log("Player went Left");
     }
     if (keyCode == "39") {
-        move.Right();
+        player.right();
         console.log("Player went Right");
     }
     //block image keys
     if (keyCode == "49") {
         blockInfo.currentImg = "ground.png";
-        console.log("Block type set to: "+blockInfo.currentImg);
+        console.log("Block type set to: " + blockInfo.currentImg);
     }
     if (keyCode == "50") {
         blockInfo.currentImg = "cloud.jpg";
-        console.log("Block type set to: "+blockInfo.currentImg);
+        console.log("Block type set to: " + blockInfo.currentImg);
     }
     if (keyCode == "51") {
         blockInfo.currentImg = "yellow_wall.png";
-        console.log("Block type set to: "+blockInfo.currentImg);
+        console.log("Block type set to: " + blockInfo.currentImg);
     }
     if (keyCode == "52") {
         blockInfo.currentImg = "roof.jpg";
-        console.log("Block type set to: "+blockInfo.currentImg);
+        console.log("Block type set to: " + blockInfo.currentImg);
     }
     if (keyCode == "53") {
         blockInfo.currentImg = "dark_green.png";
-        console.log("Block type set to: "+blockInfo.currentImg);
+        console.log("Block type set to: " + blockInfo.currentImg);
     }
     if (keyCode == "54") {
         blockInfo.currentImg = "light_green.png";
-        console.log("Block type set to: "+blockInfo.currentImg);
+        console.log("Block type set to: " + blockInfo.currentImg);
     }
     if (keyCode == "55") {
         blockInfo.currentImg = "wall.jpg";
-        console.log("Block type set to: "+blockInfo.currentImg);
+        console.log("Block type set to: " + blockInfo.currentImg);
     }
     if (keyCode == "56") {
         blockInfo.currentImg = "trunk.jpg";
-        console.log("Block type set to: "+blockInfo.currentImg);
+        console.log("Block type set to: " + blockInfo.currentImg);
     }
     if (keyCode == "57") {
         blockInfo.currentImg = "unique.png";
-        console.log("Block type set to: "+blockInfo.currentImg);
+        console.log("Block type set to: " + blockInfo.currentImg);
     }
 }
